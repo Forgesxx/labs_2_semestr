087b67e3f85251b80c332e94fa746fbc63b4dd32
ROG\forge <Forgesx236@gmail.com>
2024-03-30 17:39:53 +0200

    time measuring has been implemented

--- Program.cs
+++ Program.cs
@@ -3,17 +3,26 @@ using static InsertionSorter;
 using static BinarySearchSorter;
 using static BinarySearchCycleSorter;
 
-int[] arr = Common.generateArray(10);
-Console.WriteLine($"arr: {Common.arrToString(arr)}");
+int[] arr = Common.generateArray(10000);
+// Console.WriteLine($"arr: {Common.arrToString(arr)}");
 
+var watch = System.Diagnostics.Stopwatch.StartNew();
 InsertionSorter sorter = new InsertionSorter();
 int[] sortedArr = sorter.sort(arr);
-Console.WriteLine($"SortedArr: {Common.arrToString(sortedArr)}");
+// Console.WriteLine($"SortedArr: {Common.arrToString(sortedArr)}");
+watch.Stop();
+Console.WriteLine($"time:{watch.ElapsedMilliseconds} ms");
 
+watch = System.Diagnostics.Stopwatch.StartNew();
 sorter = new BinarySearchSorter();
 int[] sortedArrBinary = sorter.sort(arr);
-Console.WriteLine($"SortedArrBinary: {Common.arrToString(sortedArrBinary)}");
+// Console.WriteLine($"SortedArrBinary: {Common.arrToString(sortedArrBinary)}");
+watch.Stop();
+Console.WriteLine($"time:{watch.ElapsedMilliseconds} ms");
 
+watch = System.Diagnostics.Stopwatch.StartNew();
 sorter = new BinarySearchCycleSorter();
 int[] sortedArrCycleBinary = sorter.sort(arr);
-Console.WriteLine($"SortedArrCycleBinary: {Common.arrToString(sortedArrCycleBinary)}");
+// Console.WriteLine($"SortedArrCycleBinary: {Common.arrToString(sortedArrCycleBinary)}");
+watch.Stop();
+Console.WriteLine($"time:{watch.ElapsedMilliseconds} ms");
